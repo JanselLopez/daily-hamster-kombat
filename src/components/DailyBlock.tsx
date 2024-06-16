60267b950c8ec756aec63487b4f67badf0370c6b
JanselLopez <95940582+JanselLopez@users.noreply.github.com>
2024-06-16 07:43:13 -0700

    Adding my referal link

--- src/components/DailyBlock.tsx
+++ src/components/DailyBlock.tsx
@@ -14,7 +14,7 @@ const DailyBlock = ({date,cypher,comboCard1,comboCard2,comboCard3}:{
     <div className='md:grid md:grid-cols-2 w-screen relative'>
       <div className="absolute w-full text-center pt-4 text-6xl font-semibold">{date}</div>
       <ScrollDesktopContainer/>
-      <img src={`${window.location.href}/icon.webp`} className="transition-all absolute w-full h-full object-contain scale-[0.3] md:scale-[0.15] xl:scale-[0.2]"/>
+      <img onClick={()=>window.open('https://t.me/hamster_kOmbat_bot/start?startapp=kentId1666436655')} src={`${window.location.href}/icon.webp`} className="cursor-pointer hover:brightness-125 transition-all absolute w-full h-full object-contain scale-[0.3] md:scale-[0.15] xl:scale-[0.2]"/>
       <MorseCodeBlock word={cypher}/>
       <ComboCardsBlock cards={[comboCard1, comboCard2, comboCard3]}/>
     </div>
